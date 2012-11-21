86fecb68edad8a0e7dc20e74c85f59c63701a5d3
James McConnell <james.mcconnell@ilmservice.com>
2012-11-21 11:41:08 -0600

    Turning view models into function literals.

--- ExpenseTracker/Scripts/app.js
+++ ExpenseTracker/Scripts/app.js
@@ -38,3 +38,20 @@ app.dataservice = (function (app) {
         submitPaycheckBudget: submitPaycheckBudget
     };
 })(app);
+
+app.utilities = (function (app) {
+	"use strict";
+	var formatCurrency = function (value) {
+		return '$' + value.toFixed(2);
+	};
+
+	var formatDate = function (value) {
+		var d = Date.parse(value);
+		return d.toString("M/d/yyyy");
+	};
+
+	return {
+		formatCurrency: formatCurrency,
+		formatDate: formatDate
+	};
+})(app);
--- ExpenseTracker/Scripts/ko-models.js
+++ ExpenseTracker/Scripts/ko-models.js
@@ -1,109 +1,122 @@
-﻿function formatCurrency(value) {
-	return '$' + value.toFixed(2);
-}
+﻿$(function () {
+	// Budget Item view model
+	app.PaycheckBudgetItem = function () {
+		var self = this;
+		self.PaycheckBudgetItemId = ko.observable();
+		self.PaycheckBudgetId = ko.observable();
+		self.Description = ko.observable();
+		self.Amount = ko.observable();
+		self.PaidDate = ko.observable();
+		self.IsPaid = ko.observable();
 
-function formatDate(value) {
-	var d = Date.parse(value);
-	return d.toString("M/d/yyyy");
-}
+		self.loadData = function (data) {
+			self.PaycheckBudgetItemId(data.PaycheckBudgetItemId);
+			self.PaycheckBudgetId(data.PaycheckBudgetId);
+			self.Description(data.Description);
+			self.Amount(data.Amount);
+			self.PaidDate(data.PaidDate);
+			self.IsPaid(data.IsPaid);
+		};
 
-var PaycheckBudgetItem = function () {
-	var self = this;
-	self.PaycheckBudgetItemId = ko.observable();
-	self.PaycheckBudgetId = ko.observable();
-	self.Description = ko.observable();
-	self.Amount = ko.observable();
-	self.PaidDate = ko.observable();
-	self.IsPaid = ko.observable();
+		self.updateStatus = function (item) {
+			item.IsPaid(true);
+			app.dataservice.updatePaycheckBudgetItem(item.PaycheckBudgetItemId(), ko.toJSON(item));
+		};
 
-	self.loadData = function (data) {
-		self.PaycheckBudgetItemId(data.PaycheckBudgetItemId);
-		self.PaycheckBudgetId(data.PaycheckBudgetId);
-		self.Description(data.Description);
-		self.Amount(data.Amount);
-		self.PaidDate(data.PaidDate);
-		self.IsPaid(data.IsPaid);
-	};
+		return {
+			loadData: loadData,
+			updateStatus: updateStatus
+		};
+	} ();
 
-	self.updateStatus = function (item) {
-		item.IsPaid(true);
-		app.dataservice.updatePaycheckBudgetItem(item.PaycheckBudgetItemId(), ko.toJSON(item));
-	};
-};
-
-var PaycheckBudget = function () {
-	var self = this;
-	self.PaycheckBudgetId = ko.observable();
-	self.PaycheckBudgetDate = ko.observable();
-	self.PaycheckBudgetAmount = ko.observable();
-	self.PaycheckBudgetItems = ko.observableArray([]);
-	self.RemainingAmount = ko.computed(function () {
-		if (self.PaycheckBudgetItems().length > 0) {
-			var itemSum = 0;
-			ko.utils.arrayForEach(self.PaycheckBudgetItems(), function (item) {
-				itemSum += item.Amount();
-			});
-			return self.PaycheckBudgetAmount() - itemSum;
-		} else {
-			return 0;
-		}
-	});
-
-	self.loadData = function (data) {
-		self.PaycheckBudgetId(data.PaycheckBudgetId);
-		self.PaycheckBudgetDate(formatDate(data.PaycheckBudgetDate));
-		self.PaycheckBudgetAmount(data.PaycheckBudgetAmount);
-		self.PaycheckBudgetItems(ko.utils.arrayMap(data.PaycheckBudgetItems, function (item) {
-			var budgetItem = new PaycheckBudgetItem();
-			budgetItem.loadData(item);
-			return budgetItem;
-		}));
-	};
-
-	self.addItem = function () {
-		self.Items.push(new PaycheckBudgetItem());
-		// This attaches the jQuery UI datepicker to the dynamically created input.
-		$('.item-duedate:not(.hasDatePicker)').datepicker({
-			showOn: 'button',
-			buttonImage: 'images/calendar.gif',
-			buttonImageOnly: true
+	app.PaycheckBudget = function () {
+		var self = this;
+		self.PaycheckBudgetId = ko.observable();
+		self.PaycheckBudgetDate = ko.observable();
+		self.PaycheckBudgetAmount = ko.observable();
+		self.PaycheckBudgetItems = ko.observableArray([]);
+		self.RemainingAmount = ko.computed(function () {
+			if (self.PaycheckBudgetItems().length > 0) {
+				var itemSum = 0;
+				ko.utils.arrayForEach(self.PaycheckBudgetItems(), function (item) {
+					itemSum += item.Amount;
+				});
+				return self.PaycheckBudgetAmount() - itemSum;
+			} else {
+				return 0;
+			}
 		});
-	};
 
-	self.removeItem = function (item) {
-		self.Items.remove(item);
-	};
-};
+		self.loadData = function (data) {
+			self.PaycheckBudgetId(data.PaycheckBudgetId);
+			self.PaycheckBudgetDate(app.utilities.formatDate(data.PaycheckBudgetDate));
+			self.PaycheckBudgetAmount(data.PaycheckBudgetAmount);
+			self.PaycheckBudgetItems(ko.utils.arrayMap(data.PaycheckBudgetItems, function (item) {
+				var budgetItem = app.PaycheckBudgetItem.loadData(item);
+				return budgetItem;
+			}));
+		};
 
-var BudgetListViewModel = function (data) {
-	var self = this;
-	self.budgets = ko.observableArray(ko.utils.arrayMap(data, function (item) {
-		var budget = new PaycheckBudget();
-		budget.loadData(item);
-		return budget;
-	}));
+		self.addItem = function () {
+			self.Items.push(new PaycheckBudgetItem());
+			// This attaches the jQuery UI datepicker to the dynamically created input.
+			$('.item-duedate:not(.hasDatePicker)').datepicker({
+				showOn: 'button',
+				buttonImage: 'images/calendar.gif',
+				buttonImageOnly: true
+			});
+		};
 
-	self.selectedBudget = ko.observable(new PaycheckBudget());
+		self.removeItem = function (item) {
+			self.Items.remove(item);
+		};
 
-	self.editPaycheck = function (pb) {
-		//$('#edit-paycheckbudget-modal').modal('show');
-		self.selectedBudget(pb);
-	};
+		return {
+			loadData: loadData,
+			addItem: addItem,
+			removeItem: removeItem
+		};
+	} ();
 
-	self.addPaycheck = function () {
-		//$('#edit-paycheckbudget-modal').modal('show');
-		self.selectedBudget(new PaycheckBudget());
-	};
+	app.BudgetListViewModel = function () {
+		var self = this;
+		self.budgets = ko.observableArray([]);
+		self.selectedBudget = ko.observable();
 
-	self.submitData = function (item) {
-		app.dataservice.submitPaycheckBudget(
-            ko.toJSON(item),
-            function (result) {
-            	$('#edit-paycheckbudget-modal').modal('hide');
-            },
-            function (xhr) {
-            	alert(xhr.statusText + " " + xhr.repsonseText);
-            }
-        );
-	};
-};
+		self.loadData = function (data) {
+			self.budgets(ko.utils.arrayMap(data, function (item) {
+				var budget = app.PaycheckBudget.loadData(item);
+				return budget;
+			}));
+		};
+
+		self.editPaycheck = function (pb) {
+			self.selectedBudget(pb);
+		};
+
+		self.addPaycheck = function () {
+			self.selectedBudget(new app.PaycheckBudget());
+		};
+
+		self.submitData = function (item) {
+			app.dataservice.submitPaycheckBudget(
+				ko.toJSON(item),
+				function (result) {
+					$('#edit-paycheckbudget-modal').modal('hide');
+				},
+				function (xhr) {
+					alert(xhr.statusText + " " + xhr.responseText);
+				}
+			);
+		};
+
+		return {
+			budgets: budgets,
+			selectedBudget: selectedBudget,
+			loadData: loadData,
+			editPaycheck: editPaycheck,
+			addPaycheck: addPaycheck,
+			submitData: submitData
+		}
+	} ();
+});
